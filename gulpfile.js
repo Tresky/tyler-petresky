8e9789a8820073003056f5b530583787a8a59a92
TylerPetresky <tnpetresky@gmail.com>
2015-07-17 17:45:31 -0700

    Added gulpjs and browsersync

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var browser_sync = require('browser-sync').create();
+var nodemon = require('gulp-nodemon');
+var reload = browser_sync.reload;
+
+gulp.task('default', ['nodemon', 'browser-sync'], function () {
+});
+
+gulp.task('browser-sync' , function() {
+    var files = [
+        'public/**/*.*'
+    ];
+
+    browser_sync.init(files, {
+        proxy: 'http://localhost:3000',
+        port: 4000
+    });
+});
+
+gulp.task('nodemon', function(cb) {
+    var started = false;
+
+    return nodemon({
+        script: 'app.js'
+    }).on('start', function() {
+        if (!started) {
+            cb();
+            started = true;
+        }
+    });
+});
